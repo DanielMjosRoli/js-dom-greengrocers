dbf4186695f28b3bf7bf2ec30c57c674b4c04908
DanielMjosRoli <daniel.mrfush@gmail.com>
2024-09-24 10:23:13 +0200

    Done with core, might come back to do extensions

--- index.js
+++ index.js
@@ -53,3 +53,70 @@ const state = {
   ],
   cart: []
 };
+
+function removeFromCart(itemName){
+  let element = state.cart.find( e => e.name === itemName)
+  element.amount += -1
+  if (element.amount === 0){
+    state.cart.splice(state.cart.indexOf(element), 1)
+  }
+  renderCart()
+}
+function addToCart(itemName){
+
+  let element = state.cart.find( e => e.name === itemName)
+  if (element !== undefined){
+    if (element.name === itemName){
+      element.amount += 1
+    }
+  }else{
+    state.items.forEach((element) => {
+      if (element.name === itemName){
+        let newElement = element
+        newElement.amount = 1
+        state.cart.push(element)
+      }
+    })
+  }
+  renderCart()
+}
+function renderCart(){
+  var target = document.getElementById("shoppingCart")
+  target.innerHTML = ''
+  state.cart.forEach((element) => {
+    target.innerHTML +=`<li>
+      <img
+        class="cart--item-icon"
+        src="assets/icons/${element.id}.svg"
+        alt=${element.name}
+      />
+      <p>${element.name}</p>
+      <button onclick="removeFromCart('${element.name}')" class="quantity-btn remove-btn center">-</button>
+      <span class="quantity-text center">${element.amount}</span>
+      <button onclick="addToCart('${element.name}')"class="quantity-btn add-btn center">+</button>
+    </li>`
+  })
+  getTotalCost()
+}
+function renderStoreItems(){
+  var target = document.getElementById("shopItems")
+  target.innerHTML = ''
+  state.items.forEach((element) => {
+    target.innerHTML += `<li>
+          <div class="store--item-icon">
+            <img src="assets/icons/${element.id}.svg" alt=${element.name} />
+          </div>
+          <button onclick="addToCart('${element.name}')">Add to cart</button>
+        </li>`
+  })
+}
+function getTotalCost(){
+  var target = document.getElementById("cost")
+    target.innerHTML =''
+  let sum = 0
+  state.cart.forEach((element) => {
+    sum += element.price * element.amount
+  })
+  target.innerHTML += `<span id="cost" class="total-number">${sum.toFixed(2)}£</span>`
+}
+renderStoreItems()
